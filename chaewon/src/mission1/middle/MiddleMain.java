cdd17797a06f08aa2288d1f491de2325b598f33a
chaewonJo <ch730268@gmail.com>
2024-11-13 02:55:40 +0900

    feat: 미션 1 초급, 중급 수행

---
+++ chaewon/src/mission1/middle/MiddleMain.java
@@ -0,0 +1,17 @@
+package mission1.middle;
+
+public class MiddleMain {
+    public static void main(String[] args) {
+        Student[] students = {
+                Student.of("학생1", 18, 90),
+                Student.of("학생2", 19, 80),
+                Student.of("학생3", 17, 75),
+                Student.of("학생4", 20, 95),
+                Student.of("학생5", 22, 60)
+        };
+
+        for (Student student : students) {
+            System.out.println("학생 이름: " + student.getName() + " | 나이: " + student.getAge() + " | 성적: " + student.getGrade());
+        }
+    }
+}
---
+++ chaewon/src/mission1/middle/Student.java
@@ -0,0 +1,29 @@
+package mission1.middle;
+
+public class Student {
+    private final String name;
+    private final int age;
+    private final int grade;
+
+    public Student(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public static Student of(String name, int age, int grade){
+        return new Student(name, age, grade);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+}
