7589a515c2929331fc1efb30ba2e0d7bb2dc3cbe
ljh468 <ljh468@naver.com>
2024-11-13 23:07:28 +0900

    fix: print format modify

--- jaehoon/src/section02/StudentTest.java
+++ jaehoon/src/section02/StudentTest.java
@@ -7,13 +7,16 @@ public class StudentTest {
         new Student("윤정년", 19, "B+"),
         new Student("허영서", 19, "A"),
         new Student("서혜랑", 34, "S"),
-        new Student("서혜랑", 34, "S"),
+        new Student("문옥경", 34, "S+"),
         new Student("강소복", 43, "S")
     };
 
     System.out.println("### 학생 정보 ###");
     for (Student student : students) {
-      System.out.println("이름: " + student.getName() + ", 나이: " + student.getAge() + ", 등급: " + student.getGrade());
+      System.out.println(String.format("이름: %s, 나이: %d, 등급: %s",
+                                       student.getName(),
+                                       student.getAge(),
+                                       student.getGrade()));
     }
 
   }
