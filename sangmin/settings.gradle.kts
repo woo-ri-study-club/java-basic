9b38b8bf324051f3adf20f51993cc62a73309731
Sangmin <sm9171@nate.com>
2024-11-13 10:08:04 +0900

    feat: section3 - mission1 구현

---
+++ sangmin/settings.gradle.kts
@@ -0,0 +1,12 @@
+include("section2:mission1")
+findProject(":section2:mission1")?.name = "mission1"
+include("section2:mission2")
+findProject(":section2:mission2")?.name = "mission2"
+include("section2:mission3")
+findProject(":section2:mission3")?.name = "mission3"
+include("section3:mission2")
+findProject(":section3:mission2")?.name = "mission2"
+include("section3:mission3")
+findProject(":section3:mission3")?.name = "mission3"
+include("section3:mission3")
+findProject(":section3:mission3")?.name = "mission3"
