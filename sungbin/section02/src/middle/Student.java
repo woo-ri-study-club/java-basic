687ceda84f26f7a4026b876eb4809a618ff8f1b7
SungbinYang <ysb05222@naver.com>
2024-11-13 20:48:54 +0900

    feat: 섹션2 미션 중급레벨 완료

---
+++ sungbin/section02/src/middle/Student.java
@@ -0,0 +1,20 @@
+package middle;
+
+public class Student {
+    private String name;
+
+    private int age;
+
+    private String grade;
+
+    public Student(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "이름: " + name + ", 나이: " + age + ", 성적: " + grade;
+    }
+}
