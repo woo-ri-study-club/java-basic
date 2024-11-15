d50bea571a9080ece8094bd5167662b7e9cbfcf4
ljh468 <ljh468@naver.com>
2024-11-16 08:12:25 +0900

    feature: add section06 mission with package

---
+++ jaehoon/src/section06/example/UtilityTest.java
@@ -0,0 +1,11 @@
+package section06.example;
+
+import static section06.example.utility.MathHelper.add;
+
+public class UtilityTest {
+
+  public static void main(String[] args) {
+    int add = add(7, 10);
+    System.out.println("add = " + add);
+  }
+}
---
+++ jaehoon/src/section06/example/utility/MathHelper.java
@@ -0,0 +1,8 @@
+package section06.example.utility;
+
+public class MathHelper {
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+}
