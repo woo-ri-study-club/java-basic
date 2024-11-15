be2c70da420e621a0c2f11b63b7f8325749df7e5
nahye <nnhh.shine@gmail.com>
2024-11-15 16:08:50 +0900

    미션3 고급문제 완료

--- nahye/mission03/Book.java
+++ nahye/mission03/Book.java
@@ -6,13 +6,21 @@ public class Book {
     private String title;
     private String author;
     private String isBn;
+    private boolean isCheckedOut;
 
     public Book(String title, String author, String isBn) {
         this.title = title;
         this.author = author;
         this.isBn = isBn;
+        this.isCheckedOut = false;
     }
 
+    public Book(String title, String author, String isBn, boolean isCheckedOut) {
+        this.title = title;
+        this.author = author;
+        this.isBn = isBn;
+        this.isCheckedOut = isCheckedOut;
+    }
 
     public String getTitle() {
         return title;
@@ -26,9 +34,17 @@ public class Book {
         return isBn;
     }
 
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
     @Override
     public String toString() {
-        return "제목: "+title+", 저자: "+author+", ISBN: "+isBn;
+        return "제목: " + title + ", 저자: " + author + ", ISBN: " + isBn;
     }
 
     @Override
