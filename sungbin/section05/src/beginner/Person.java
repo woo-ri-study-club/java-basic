807947e3bbd116fab2675a6b75e354d57ba50188
SungbinYang <ysb05222@naver.com>
2024-11-17 08:48:03 +0900

    feat. 섹션5 미션 초급 레벨 제출

---
+++ sungbin/section05/src/beginner/Person.java
@@ -0,0 +1,18 @@
+package beginner;
+
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    public Person() {
+        this.name = "unknown";
+        this.age = 0;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
