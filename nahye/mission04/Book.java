21cd7ac017e339bf6c8051575cbb721fe9c0e685
nahye <nnhh.shine@gmail.com>
2024-11-17 19:11:32 +0900

    미션4 수정
    - 생성자에 예외처리 추가
    - addBook() 에 findEmptyIndex() 추가해서 분리
    -searchBookByTitle()에 findBookByTitle()추가해서 분리

--- nahye/mission04/Book.java
+++ nahye/mission04/Book.java
@@ -1,23 +1,38 @@
 package mission04;
 
+import java.util.Objects;
+
 public class Book {
 
+    private static final int DEFAULT_YEAR = 2024;
     private String title;
     private String author;
     private int publishedYear;
 
+
     public Book(String title, String author) {
-        this(title, author, 2024);
+        this(title, author, DEFAULT_YEAR);
     }
 
     public Book(String title, String author, int publishedYear) {
+
+        if (title == null || title.trim().isEmpty()) {
+            throw new IllegalArgumentException("제목은 필수 입력값입니다.");
+        }
+        if (author == null || author.trim().isEmpty()) {
+            throw new IllegalArgumentException("저자는 필수 입력값입니다.");
+        }
+        if (publishedYear < 0) {
+            throw new IllegalArgumentException("출판연도는 0보다 작을 수 없습니다.");
+        }
+
         this.title = title;
         this.author = author;
         this.publishedYear = publishedYear;
     }
 
     public boolean hasSameTitle(String title) {
-        return this.title.equals(title);
+        return Objects.equals(this.title, title);
     }
 
     @Override
--- nahye/mission04/Library.java
+++ nahye/mission04/Library.java
@@ -1,23 +1,39 @@
 package mission04;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+
 public class Library {
     private Book[] books;
 
     public Library() {
-        this.books = new Book[10];
+        this(10);
     }
+
     public Library(int length) {
+        if (length < 0) {
+            throw new IllegalArgumentException("0보다 작을 수 없습니다.");
+        }
         this.books = new Book[length];
     }
 
-    public void addBook(String title, String author, int publishedYear) {
+    public Optional<Integer> findEmptyIndex() {
         for (int i = 0; i < books.length; i++) {
-            if (books[i] == null) {
-                books[i] = new Book(title, author, publishedYear);
-                return;
-            }
+            if (books[i] == null) return Optional.of(i);
+        }
+        return Optional.empty();
+    }
+
+    public boolean addBook(String title, String author, int publishedYear) {
+        Optional<Integer> emptyIndex = findEmptyIndex();
+
+        if (findEmptyIndex().isPresent()) {
+            books[emptyIndex.get()] = new Book(title, author, publishedYear);
+            return true;
         }
         System.out.println("더이상 도서를 추가 할 수 없습니다.");
+        return false;
     }
 
     public void displayBooks() {
@@ -29,17 +45,19 @@ public class Library {
 
     public void searchBookByTitle(String title) {
         System.out.println("=== '" + title + "' 검색 결과 ===");
-        boolean find = false;
 
-        for (Book book : books) {
-            if (book != null && book.hasSameTitle(title)) {
-                System.out.println(book);
-                find = true;
-            }
-        }
-        if (find == false) {
-            System.out.println("해당 도서가 없습니다.");
-        }
+        Optional<Book> findBook = findBookByTitle(title);
+        findBook.ifPresentOrElse(
+                book -> System.out.println(book),
+                () -> System.out.println("해당 도서가 없습니다.")
+        );
+    }
+
+    public Optional<Book> findBookByTitle(String title) {
+        return Arrays.stream(books)
+                .filter(Objects::nonNull)
+                .filter(book -> book.hasSameTitle(title))
+                .findFirst();
     }
 
 }
