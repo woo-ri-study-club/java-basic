8b567ebe87268297dfb889c3bd7a93347e2df902
nahye <nnhh.shine@gmail.com>
2024-11-18 01:40:04 +0900

    미션 5 중급 완료

---
+++ nahye/mission05/Main.java
@@ -0,0 +1,10 @@
+package mission05;
+
+import static mission05.com.example.utility.MathHelper.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = add(2,3);
+        System.out.println(result);
+    }
+}
