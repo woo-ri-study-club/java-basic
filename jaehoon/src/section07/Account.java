8f634f15a9019558fac210a88beacfae74bf8fda
ljh468 <ljh468@naver.com>
2024-11-19 08:17:08 +0900

    fix: change 메서드 출력문 위치 수정

--- jaehoon/src/section07/Account.java
+++ jaehoon/src/section07/Account.java
@@ -10,18 +10,22 @@ public class Account {
     this.user = user;
   }
 
+  // 계정 ID 가져오기
   public String getAccountId() {
     return accountId;
   }
 
+  // User의 닉네임 가져오기
   public String getNickname() {
     return user.getName();
   }
 
+  // User 이름 변경 요청
   public void requestUserNameChange(String newName) {
     user.changeName(newName);
   }
 
+  // 비밀번호 변경 요청
   public void requestPasswordChange(String oldPassword, String newPassword) {
     user.changePassword(oldPassword, newPassword);
   }
--- jaehoon/src/section07/User.java
+++ jaehoon/src/section07/User.java
@@ -21,12 +21,14 @@ public class User {
       throw new IllegalArgumentException("이름은 비어 있거나 공백이 을 수 없습니다.");
     }
     this.name = newName;
+    System.out.println("이름이 성공적으로 변경되었습니다.");
   }
 
   public void changePassword(String oldPassword, String newPassword) {
     validateOldPassword(oldPassword);
     validateNewPassword(newPassword);
     this.password = newPassword;
+    System.out.println("비밀번호가 성공적으로 변경되었습니다.");
   }
 
   private void validateOldPassword(String oldPassword) {
@@ -40,5 +42,4 @@ public class User {
       throw new IllegalArgumentException("새 비밀번호는 비어 있거나 공백일 수 없습니다.");
     }
   }
-
 }
