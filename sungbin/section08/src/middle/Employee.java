cb0d62f47dcc46ebcfdd69a3a4cef8248842c411
SungbinYang <ysb05222@naver.com>
2024-11-20 00:24:28 +0900

    feat. 섹션8 중급레벨 제출

---
+++ sungbin/section08/src/middle/Employee.java
@@ -0,0 +1,21 @@
+package middle;
+
+public class Employee {
+
+    private long id;
+
+    private String name;
+
+    public Employee(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
