c12821bc804297745e16b7b236dd8de4866bcccd
hmkim199 <hmkim199@gmail.com>
2024-11-20 23:08:49 +0900

    feat: 미션 9 - 초급

---
+++ hyemin/src/main/java/mission9/Animal.java
@@ -0,0 +1,7 @@
+package mission9;
+
+public class Animal {
+    public void sound() {
+        System.out.println("동물을 소리를 낼 수 있다.");
+    }
+}
---
+++ hyemin/src/main/java/mission9/Dog.java
@@ -0,0 +1,8 @@
+package mission9;
+
+public class Dog extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("왈왈!");
+    }
+}
