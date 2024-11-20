8773b1eb9d65785fc4e0d22280ddd6d982973042
hmkim199 <hmkim199@gmail.com>
2024-11-20 23:16:02 +0900

    feat: 미션 9 - 중급

---
+++ hyemin/src/main/java/mission9/Car.java
@@ -0,0 +1,14 @@
+package mission9;
+
+public class Car extends Vehicle {
+    private String brand;
+
+    public Car(int maxSpeed, String brand) {
+        super(maxSpeed);
+        this.brand = brand;
+    }
+
+    public void showDetails() {
+        System.out.println("Brand: " + brand + ", Max Speed: " + getMaxSpeed());
+    }
+}
