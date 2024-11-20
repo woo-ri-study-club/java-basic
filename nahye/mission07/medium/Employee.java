e243672ac8b07585caf33cbd3aba826faa98666e
nahye <nnhh.shine@gmail.com>
2024-11-20 18:56:53 +0900

    미션 7 중급 수정

--- nahye/mission07/medium/Employee.java
+++ nahye/mission07/medium/Employee.java
@@ -3,15 +3,13 @@ package mission07.medium;
 public class Employee {
     private String name;
     private int id;
-    private static int employeeCount;
 
     public Employee(String name, int id) {
         this.name = name;
         this.id = id;
-        employeeCount++;
     }
 
     public static int getTotalEmployees(){
-        return employeeCount;
+        return EmployeeManager.employeeCount;
     }
 }
--- nahye/mission07/medium/Main.java
+++ nahye/mission07/medium/Main.java
@@ -2,9 +2,11 @@ package mission07.medium;
 
 public class Main {
     public static void main(String[] args) {
-        Employee emp1 = new Employee("John", 101);
-        Employee emp2 = new Employee("Jane", 102);
-        Employee emp3 = new Employee("Mike", 103);
+        EmployeeManager employeeManager = new EmployeeManager();
+
+        Employee emp1 = employeeManager.add("John", 101);
+        Employee emp2 = employeeManager.add("Jane", 102);
+        Employee emp3 = employeeManager.add("Mike", 103);
 
         System.out.println("총 직원 수: " + Employee.getTotalEmployees());
     }
