e2a7930f8f32bd7fe197b92c242ca36b8c9cc6e6
nahye <nnhh.shine@gmail.com>
2024-11-20 21:15:32 +0900

    미션 7 고급 예외처리 추가

--- nahye/mission07/advanced/ConfigurationManager.java
+++ nahye/mission07/advanced/ConfigurationManager.java
@@ -19,16 +19,25 @@ public class ConfigurationManager {
     }
 
     public void setConfig(String key, String value) {
-        if (key == null) {
-            throw new IllegalArgumentException("Key가 없습니다.");
-        }
+        checkKeyNotNull(key);
         configurations.put(key,value);
     }
 
     public String getConfig(String key) {
+        checkKeyNotNull(key);
+        validateKeyContained(key);
+        return configurations.get(key);
+    }
+
+    private void checkKeyNotNull(String key) {
         if (key == null) {
-            throw new IllegalArgumentException("Key가 없습니다.");
+            throw new IllegalArgumentException("Key는 null일 수 없습니다.");
         }
-       return configurations.get(key);
+    }
+
+    private void validateKeyContained(String key) {
+        if (!configurations.containsKey(key)) {
+            throw new IllegalArgumentException(key+"를 찾을 수 없습니다.");
+        }
     }
 }
