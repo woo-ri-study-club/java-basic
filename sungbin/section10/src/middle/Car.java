0c7981e744ccded069d7b1f134346beb862b0131
SungbinYang <ysb05222@naver.com>
2024-11-22 00:51:02 +0900

    feat. 섹션10 미션 중급레벨 제출

---
+++ sungbin/section10/src/middle/Car.java
@@ -0,0 +1,15 @@
+package middle;
+
+public class Car extends Vehicle {
+
+    private String brand;
+
+    public Car(String brand, int maxSpeed) {
+        super(maxSpeed);
+        this.brand = brand;
+    }
+
+    public void showDetails() {
+        System.out.println("Brand: " + brand + ", Max Speed: " + maxSpeed);
+    }
+}
