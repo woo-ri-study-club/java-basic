caf60669f281cc3db87084ba6d6580c568a19ab6
nahye <nnhh.shine@gmail.com>
2024-11-21 22:33:53 +0900

    미션 9 초급 완료

---
+++ nahye/mission09/basic/Dog.java
@@ -0,0 +1,8 @@
+package mission09.basic;
+
+public class Dog extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("왈왈!");
+    }
+}
---
+++ nahye/mission09/basic/Main.java
@@ -0,0 +1,8 @@
+package mission09.basic;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.sound();
+    }
+}
