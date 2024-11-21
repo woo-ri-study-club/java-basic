451600a5daa7a510e18ce257f1a8620b8e40b5ab
seuem <lazyslug@kakao.com>
2024-11-21 13:19:32 +0900

    [이세음]미션8 제출

--- sangmin/settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-include("section2:mission1")
-findProject(":section2:mission1")?.name = "mission1"
-include("section2:mission2")
-findProject(":section2:mission2")?.name = "mission2"
-include("section2:mission3")
-findProject(":section2:mission3")?.name = "mission3"
