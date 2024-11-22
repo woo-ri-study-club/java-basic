a38b7c9e887950fd157aacaa6df814f9f5822d50
hmkim199 <hmkim199@gmail.com>
2024-11-22 23:54:07 +0900

    feat: 미션 10 - 고급

---
+++ hyemin/src/main/java/mission10/Circle.java
@@ -0,0 +1,15 @@
+package mission10;
+
+public class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
