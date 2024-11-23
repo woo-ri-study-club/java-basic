03f41a05ab920dcada18a9ede29e8aae84c28937
SungbinYang <ysb05222@naver.com>
2024-11-24 00:10:08 +0900

    feat.섹션12 초급레벨 미션 제출

---
+++ sungbin/section12/src/beginner/Rectangle.java
@@ -0,0 +1,18 @@
+package beginner;
+
+public class Rectangle extends Shape {
+
+    private double width;
+
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+}
---
+++ sungbin/section12/src/beginner/Shape.java
@@ -0,0 +1,6 @@
+package beginner;
+
+public abstract class Shape {
+
+    public abstract double getArea();
+}
