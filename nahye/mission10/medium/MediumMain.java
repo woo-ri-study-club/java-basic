c5120bfc487aecff8e0a169b5114d9a07ca27de6
nahye <nnhh.shine@gmail.com>
2024-11-23 22:59:30 +0900

    미션 10 중급 수정

--- nahye/mission10/medium/MediumMain.java
+++ nahye/mission10/medium/MediumMain.java
@@ -3,14 +3,16 @@ package mission10.medium;
 public class MediumMain {
     public static void main(String[] args) {
 
-        Animal[] animals = {new Dog(), new Cat(), new Cat()};
+        Animal[] animals = {new Dog(), new Cat(), new Cat(), new Animal()};
 
         for(Animal a : animals) {
             if(a instanceof Dog) {
                 System.out.println("개입니다.");
             }
-            if(a instanceof Cat) {
+            else if(a instanceof Cat) {
                 System.out.println("고양이입니다.");
+            }else {
+                System.out.println("동물입니다.");
             }
         }
     }
