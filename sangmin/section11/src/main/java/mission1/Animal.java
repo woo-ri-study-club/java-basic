54aa5de3ffdd1b749977e10772f39fadaab99d0d
Sangmin <sm9171@nate.com>
2024-11-23 14:09:32 +0900

    feat: section11 - mission1 구현

---
+++ sangmin/section11/src/main/java/mission1/Animal.java
@@ -0,0 +1,7 @@
+package mission1;
+
+public class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
---
+++ sangmin/section11/src/main/java/mission1/Dog.java
@@ -0,0 +1,8 @@
+package mission1;
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
