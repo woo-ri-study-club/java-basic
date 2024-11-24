397629e56bed4093b70ce0485df483364ad2adaa
Sangmin <sm9171@nate.com>
2024-11-24 20:44:22 +0900

    refactor: section12 - mission2 수정

--- sangmin/section12/src/main/java/mission2/Main.java
+++ sangmin/section12/src/main/java/mission2/Main.java
@@ -2,8 +2,10 @@ package mission2;
 
 public class Main {
     public static void main(String[] args) {
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        mediaPlayer.play();
-        mediaPlayer.pause();
+        Playable play = new MediaPlayer();
+        Pauseable pause = new MediaPlayer();
+
+        play.play();
+        pause.pause();
     }
 }
