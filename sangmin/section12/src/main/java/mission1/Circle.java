ec4516e95fa613e803cac973291c98b787d853b6
Sangmin <sm9171@nate.com>
2024-11-24 14:49:00 +0900

    feat: section12 - mission1 구현

---
+++ sangmin/section12/src/main/java/mission1/Circle.java
@@ -0,0 +1,14 @@
+package mission1;
+
+public class Circle extends Shape {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
---
+++ sangmin/section12/src/main/java/mission1/Shape.java
@@ -0,0 +1,5 @@
+package mission1;
+
+public abstract class Shape {
+    public abstract double getArea();
+}
