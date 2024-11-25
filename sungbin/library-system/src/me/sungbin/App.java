7094de5e7d6d165d18074b27715ae12323822844
SungbinYang <ysb05222@naver.com>
2024-11-25 20:20:15 +0900

    feat. core 모듈 설계

--- sungbin/library-system/src/me/sungbin/App.java
+++ sungbin/library-system/src/me/sungbin/App.java
@@ -1,7 +1,10 @@
 package me.sungbin;
 
+import me.sungbin.core.LibrarySystem;
+
 public class App {
     public static void main(String[] args) {
-
+        LibrarySystem librarySystem = new LibrarySystem();
+        librarySystem.start();
     }
 }
---
+++ sungbin/library-system/src/me/sungbin/core/LibrarySystem.java
@@ -0,0 +1,8 @@
+package me.sungbin.core;
+
+public class LibrarySystem {
+
+    public void start() {
+
+    }
+}
