9ff50f81b7b1b59924d98624ab1c100851cb0403
tdronnie <senyvee9@gmail.com>
2024-11-26 20:32:35 +0900

    refactor: RegistData 필드 불변 처리

--- seeun/section13/member/data/RegistData.java
+++ seeun/section13/member/data/RegistData.java
@@ -1,29 +1,30 @@
 package section13.member.data;
 
-import java.util.Objects;
+import java.util.Optional;
 
 public class RegistData {
 
-    private String id;
-    private String name;
-    private String password;
-    private String key;
+    private final String id;
+    private final String name;
+    private final String password;
+    private final Optional<String> key;
 
     public RegistData(String id, String name, String password, String key) {
         this.id = id;
         this.name = name;
         this.password = password;
-        this.key = key;
+        this.key = Optional.ofNullable(key);
     }
 
     public RegistData(String id, String name, String password) {
         this.id = id;
         this.name = name;
         this.password = password;
+        this.key = Optional.empty();
     }
 
     public boolean hasKey() {
-        return key != null && !key.isEmpty();
+        return key.isPresent();
     }
 
     public String getId() {
@@ -39,6 +40,6 @@ public class RegistData {
     }
 
     public String getKey() {
-        return key;
+        return key.get();
     }
 }
